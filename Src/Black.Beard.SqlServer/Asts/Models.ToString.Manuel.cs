3d765e221b575f0beeaea417d0f73bb2964e09a7
gael beard <g.beard@pickup-services.com>
2023-04-25 05:48:19 +0200

    Add toString generators

--- Src/Black.Beard.SqlServer/Asts/Models.ToString.Manuel.cs
+++ Src/Black.Beard.SqlServer/Asts/Models.ToString.Manuel.cs
@@ -58,7 +58,38 @@ namespace Bb.SqlServer.Asts
 
     }
 
+    public partial class AstOnPartitions
+    {
 
+        /// <summary>
+        /// on_partitions
+        /// 	 : ON  PARTITIONS  LR_BRACKET  partition_nums  (COMMA  partition_nums)*  RR_BRACKET
+        /// </summary>
+        private void CustomToString(Writer writer)
+        {
+
+
+
+        }
+
+    }
+
+    public partial class AstFilespec
+    {
+
+        /// <summary>
+        /// filespec
+        /// 	 : LR_BRACKET  NAME  EQUAL  file_group_id  (COMMA  NEWNAME  EQUAL  file_group_id | stringtext)?  (COMMA  FILENAME  EQUAL  file_name = stringtext)?  (COMMA  SIZE  EQUAL  size = file_size)?  (COMMA  MAXSIZE  EQUAL  max = file_size | UNLIMITED)?  (COMMA  FILEGROWTH  EQUAL  growth_increment = file_size)?  (COMMA  OFFLINE)?  RR_BRACKET
+        /// </summary>
+        private void CustomToString(Writer writer)
+        {
+
+
+
+        }
+
+
+    }
 
 
 
